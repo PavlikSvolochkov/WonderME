36da3b31a68eca70fd3fc7c180597ea5d00d06c2
Paul Kriventsov <develop.iwork@gmail.com>
2013-10-04 01:26:06 +0700

    Добавлен about. Исправлены названия кнопок. Подправлены картинки лого и back_button. Добавлены несколько strings.

---
+++ src/ru/nsk/android/About.java
@@ -0,0 +1,13 @@
+package ru.nsk.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.about);
+  }
+}
