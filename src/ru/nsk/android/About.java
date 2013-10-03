8c857d46b8509f5b22ddb471cdebb46927828a82
Paul Kriventsov <develop.iwork@gmail.com>
2013-10-04 01:45:25 +0700

    Исправлены некоторые иконки и их названия.

--- src/ru/nsk/android/About.java
+++ src/ru/nsk/android/About.java
@@ -2,6 +2,7 @@ package ru.nsk.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class About extends Activity {
 
@@ -9,5 +10,9 @@ public class About extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.about);
+  }
+
+  public void back(View view) {
+
   }
 }
